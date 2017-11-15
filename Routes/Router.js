bf7f80978d23ab81ae88761a6c27cb67132e7027
Patipon Riebpradit <patipon74@gmail.com>
2017-11-15 17:55:16 +0700

    and user page to bike history page

--- Routes/Router.js
+++ Routes/Router.js
@@ -1,148 +1,139 @@
 import React from 'react'
-import { DrawerNavigator,TabNavigator, StackNavigator } from 'react-navigation';
+import {DrawerNavigator, TabNavigator, StackNavigator} from 'react-navigation';
 import AccountSetting from '../Pages/AccountSetting'
 import AuthenPage from '../Pages/AuthenPage'
-import { HomeScreen, ChatPanel, MapSetting, Profile, HistoryPage, ContactScreen, ChatMenu} from '../Pages'
-import {CommunicationRoute} from './'
-import { Icon, SearchBar } from 'react-native-elements'
-import {View, Text,Platform, StatusBar} from 'react-native'
+import {HomeScreen, ChatPanel, MapSetting, Profile, HistoryPage, ContactScreen, ChatMenu} from '../Pages'
+import {Icon} from 'react-native-elements'
+import {Platform, StatusBar} from 'react-native'
 
 const Routes = StackNavigator({
-    Login : {
-        screen : AuthenPage,
-        navigationOptions : {
-            header : null
-        }
+    Main: {
+        screen: TabNavigator({
+            Communication: {
+                screen: ChatMenu,
+                navigationOptions: {
+                    headerStyle: {
+                        backgroundColor: "#009688",
+                        shadowColor: 'transparent',
+                    },
+                    headerTitle: "Chats",
+                    headerTitleStyle: {
+                        color: 'white'
+                    },
+                    headerLeft: null,
+                    tabBarLabel: ({focus, tintColor}) => (
+                        <Icon color={tintColor} name='question-answer'/>
+                    ),
+
+                }
+            },
+            History: {
+                screen: HistoryPage,
+                navigationOptions: {
+                    tabBarLabel: ({focus, tintColor}) => (
+                        <Icon color={tintColor} name='event-note'/>
+                    ),
+                    header: null,
+                    headerLeft: null,
+                }
+            },
+
+
+            Map: {
+                screen: HomeScreen,
+                navigationOptions: {
+                    tabBarLabel: ({focused}) => (
+                        <Icon containerStyle={{marginTop: -5, marginBottom: -5}} reverse color={"#009688"}
+                              name='place'/>
+                    ),
+                    header: null
+                }
+            },
+
+            Users: {
+                path: "Users",
+                screen: ContactScreen,
+                navigationOptions: {
+                    headerTitle: "Bikes",
+                    tabBarLabel: ({focus, tintColor}) => (
+                        <Icon name='bicycle' type='font-awesome' color={tintColor}/> ),
+                    headerStyle: {
+                        backgroundColor: "#009688"
+                    },
+                    headerTitleStyle: {
+                        color: "white"
+                    },
+                    headerLeft: null,
+                }
+            },
+
+
+            Settings: {
+                screen: AccountSetting,
+                navigationOptions: {
+                    headerTitle: "Settings",
+                    tabBarLabel: ({focus, tintColor}) => ( <Icon color={tintColor} name='settings'/> ),
+                    headerStyle: {
+                        backgroundColor: "#009688"
+                    },
+                    headerTitleStyle: {
+                        color: "white"
+                    },
+                    headerLeft: null,
+                }
+
+            },
+
+            },
+            {
+                tabBarPosition: 'bottom',
+                animationEnabled: false,
+                swipeEnabled: false,
+                lazy: true,
+                tabBarOptions: {
+                    activeTintColor: "#009688",
+                    inactiveTintColor: "#BDBDBD",
+                    showIcon: false,
+
+                    style: {
+                        backgroundColor: "white",
+                    },
+                    pressOpacity: 0
+                }
+            })
     },
-  Main : {
-    screen :TabNavigator({
-      Communication: {
-        screen: ChatMenu,
-        navigationOptions: {
-          headerStyle: {
-            backgroundColor: "#009688",
-            shadowColor: 'transparent',
-            
-          },
-          headerTitle: "Chats",
-          headerTitleStyle : {
-            color : 'white'
-          },
-          headerLeft :null,
-          tabBarLabel: ({ focus,tintColor }) => (
-            <Icon color={tintColor} name='question-answer'/>
-          ),
-          
-        }
-      },
-      Users: {
-        path: "Users",
-        screen: ContactScreen,
-        navigationOptions: {
-          headerTitle: "Users",
-          tabBarLabel: ({focus,tintColor}) => ( <Icon color={tintColor} name = 'contacts'/> ),
-          headerStyle: {
-            backgroundColor: "#009688"
-          },
-          headerTitleStyle: {
-            color : "white"
-          },
-          headerLeft :null,
-        }
-      },
-      
-      
-      Map: {
-        screen: HomeScreen,
-        navigationOptions: {
-          tabBarLabel: ({ focused }) => (
-            <Icon containerStyle = {{marginTop: -5, marginBottom: -5}} reverse color={"#009688"} name='place'/>
-          ),
-          header : null
-        }
-      },
-      History: {
-        screen: HistoryPage,
-        navigationOptions: {
-          tabBarLabel: ({ focus, tintColor }) => (
-            <Icon color={tintColor} name='event-note'/>
-          ),
-          header : null,
-          headerLeft :null,
-        }
-      },
-      
-      
-      
-      Settings: {
-        screen: AccountSetting,
-        navigationOptions: {
-          headerTitle: "Settings",
-          tabBarLabel: ({ focus, tintColor}) => ( <Icon color={tintColor} name = 'settings'/> ),
-          headerStyle: {
-            backgroundColor: "#009688"
-          },
-          headerTitleStyle: {
-            color : "white"
-          },
-          headerLeft :null,
-        }
 
-      },
-
-    }, 
-    {
-      tabBarPosition: 'bottom',
-      animationEnabled: false,
-      swipeEnabled: false,
-      lazy: true,
-      tabBarOptions: {
-        activeTintColor : "#009688",
-        inactiveTintColor : "#BDBDBD",
-        showIcon : false,
-        
-        style: {
-          backgroundColor: "white",
-        },
-        pressOpacity: 0
-      }
-    })
-  },
-
-  Chat: {
-    path: 'chat/:id',
-    screen: ChatPanel,
-    navigationOptions: ({navigation}) => ({
-      title: `${navigation.state.params.name}`,
-      headerStyle: {
-        backgroundColor: '#009688',
-      },
-      headerTitleStyle: {
-        color : "white",
-        fontWeight: "normal"
-      },
-      headerTintColor : "white",
-      headerRight : <Icon name='info-outline' color = '#E0E0E0' containerStyle = {{marginRight:20}} onPress = {() => {navigation.navigate('Profile', { name: navigation.state.params.name, id : '0', phone : "0847060344"}) }}/>
-    })
-  },
-  Profile : {
-    path : 'Profile/:id',
-    screen : Profile,
-    navigationOptions: ({navigation}) => ({
-      header : null
-    })
-  },
+    Chat: {
+        path: 'chat/:id',
+        screen: ChatPanel,
+        navigationOptions: ({navigation}) => ({
+            title: `${navigation.state.params.name}`,
+            headerStyle: {
+                backgroundColor: '#009688',
+            },
+            headerTitleStyle: {
+                color: "white",
+                fontWeight: "normal"
+            },
+            headerTintColor: "white",
+            headerRight: <Icon name='info-outline' color='#E0E0E0' containerStyle={{marginRight: 20}} onPress={() => {
+                navigation.navigate('Profile', {name: navigation.state.params.name, id: '0', phone: "0847060344"})
+            }}/>
+        })
+    },
+    Profile: {
+        path: 'Profile/:id',
+        screen: Profile,
+        navigationOptions: ({navigation}) => ({
+            header: null
+        })
+    },
 
 
-
-  
-
-  
-  
-},{
-  cardStyle: {
-    paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
-}
+}, {
+    cardStyle: {
+        paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
+    }
 });
 
 export default Routes
